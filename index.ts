727f7abcfcbe0e6e494caab567d361948ea6dee1
Sarah Abu irmeileh <211027@ppu.edu.ps>
2023-08-19 07:17:01 +0300

    some changes for the uploading to EC2 instace, edit the port and adding some middleware'

--- index.ts
+++ index.ts
@@ -2,13 +2,20 @@ import express from "express"
 import bookRouter from "./routers/book.js"
 
 const app = express() 
-app.listen(3000,()=>{   // typical setup for starting the Express server 
-    console.log("The app is listening")
+
+const PORT = process.env.PORT || 5000;
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
 })
 
-app.use(express.json())
 app.use("/book",bookRouter )
 
 app.use((req, res) => {
     res.status(404).send("Wrong URL :(");
+})
+
+app.listen(3000,()=>{   // typical setup for starting the Express server 
+    console.log(`The app is listening oi port ${PORT}`)
 })
